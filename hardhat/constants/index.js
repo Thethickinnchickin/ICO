c9df1a7c6ae0bac65dccd31b9f5bf2108a5ebc93
Matt <matt.reiley@sjsu.edu>
2023-01-28 01:40:46 -0800

    first commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xB884C06BA9d1ab0B14a32327ca02533Ec2ae2771"
+
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
